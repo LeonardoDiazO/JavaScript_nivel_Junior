9ea2dbb5d46d711cb8b9a3924a7819d0e389fb28
Leonardo D <maximun_neo@hotmail.com>
2023-07-17 21:54:14 -0500

    First commit javascript

---
+++ codigo.js
@@ -0,0 +1,43 @@
+// alert('Hola, you')
+
+//Como asigno una variable dendon de javascript
+recipiente = "Papel";
+        
+alert(recipiente)
+
+// Tipos de variables
+String = "Cadena de texto"; // es una cadema de texto
+number = 1;// no va entre comillas
+Boolean = false; // es un valor entre 0-1 (true o false()
+
+undefined // Es un valor de tipo de dato que no esta la variable indeficina
+null // es un valor de tipo de dato
+NaN // es un valor no definido en la variable
+
+//Let y var son variables que si se pueden modificar pero Const NO
+var Max = 15; // es un valor en el que le asignamos a la varible
+let  numero; // es un valor que tiene alcance global
+const Number = numero; // es un valor de tipo que siempre va tener el mismo valor no se va cambiar
+
+numero = 10;
+
+// Que es el hoosting 
+let numero1 = 23, numero2 =10, numero3 =5;
+
+alert(numero1, numero2, numero3);
+
+//Declaramos let en valor null
+let numero5 = null;
+
+let numero6 = 5;
+let numero7 = 6;
+alert (numero6 * numero7);
+
+//Es una funcion que nos ayuda a guardar el valor en una caja de texto
+
+let nombre = prompt ("Hola, dime tu nombre");
+alert ("Hola " + nombre);
+
+// Operadores de Javascript
+
+// Minuto 50:19
