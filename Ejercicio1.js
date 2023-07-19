e9679f68610ec85f89204b9db60ae9800df86dc7
Leonardo D <maximun_neo@hotmail.com>
2023-07-18 23:51:59 -0500

    Second commit ejercicio1

---
+++ Ejercicio1.js
@@ -0,0 +1,16 @@
+//Tres muchachos de 23 años, entran a una heladeria 
+//a comprar un helado, pero tienen un problema que los 
+// los precios no estan al lado de cada helado, ellos quieren 
+// comprar el helado más caro con el dinero que tienen
+
+
+// Posibles soluciones, ingresar el monto y mostrar
+// cuantos helados hay con valores altos 
+// indicar el vuelto 
+
+valor1 = prompt("Hola, escriba cuanto dinero tiene el primer muchacho?");
+valor2 = prompt("Hola, escriba cuanto dinero tiene el segundo muchacho?");
+valor3 = prompt("Hola, escriba cuanto dinero tiene el tercer muchacho?");
+resultado = valor1 + valor2 + valor3;
+
+alert(resultado)
